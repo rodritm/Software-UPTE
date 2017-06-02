1fc71e1e6d560d43000df80e77d9fa3416cf7d08
Ronald Aparicio <raparicio71@gmail.com>
2017-06-02 01:18:26 -0400

    ficha 2

--- src/Ficha.java
+++ src/Ficha.java
@@ -1,10 +1,16 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Ficha {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -36,8 +42,44 @@ public class Ficha {
 		frame = new JFrame();
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setUndecorated(true);
+		frame.setBounds(0, 0, 10000, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JLabel lblEstudiantes = new JLabel("Estudiantes");
+		lblEstudiantes.setBounds(12, 12, 94, 15);
+		frame.getContentPane().add(lblEstudiantes);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(12, 32, 70, 15);
+		frame.getContentPane().add(lblNombre);
+		
+		textField = new JTextField();
+		textField.setBounds(76, 30, 584, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblApellidoPaterno = new JLabel("Apellido Paterno:");
+		lblApellidoPaterno.setBounds(678, 32, 139, 15);
+		frame.getContentPane().add(lblApellidoPaterno);
+		
+		JButton btnAtras = new JButton("Atras");
+		btnAtras.setBounds(1006, 672, 117, 25);
+		frame.getContentPane().add(btnAtras);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(805, 30, 549, 19);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Apellido Materno:");
+		lblNewLabel.setBounds(12, 58, 139, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(141, 56, 519, 19);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
 	}
 
 }
