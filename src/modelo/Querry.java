29e1cb5c2a22e5724135ff4f5d2a8512e3c53e07
AugustoBet4 <augusto.bet4@gmail.com>
2017-06-28 14:42:48 -0400

    Querrys de Reportes->Estadisticos

--- src/modelo/Querry.java
+++ src/modelo/Querry.java
@@ -8,6 +8,9 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 public class Querry {
+	//						LOGIN
+	
+	
 	public ResultSet Log(String user,String pass) throws SQLException{
 		DB_Connect con = new DB_Connect();
 		Connection conn=con.conexion();
@@ -18,31 +21,52 @@ public class Querry {
 		rs = st.executeQuery(querry);
 		return rs;
 	}
-//	public ResultSet EstGeneral() throws SQLException{
-//		
-//	}
-	public ResultSet EstGenero() throws SQLException{
+	
+	
+	//					ESTADISTICOS
+	
+	
+	public ResultSet EstGeneral(String gestion) throws SQLException{
 		DB_Connect con = new DB_Connect();
 		Connection conn=con.conexion();
 		Statement st = null;
 		ResultSet rs = null;
 		st = (Statement)conn.createStatement();
-		String querry = "SELECT genero, COUNT(genero) AS Total FROM estudiante GROUP BY (genero)";
+		String querry = "";
 		rs = st.executeQuery(querry);
 		return rs;
 	}
-	public ResultSet EstCurso(String curso, String gestion) throws SQLException{
+	public ResultSet EstGenero(String curso, String gestion) throws SQLException{
 		DB_Connect con = new DB_Connect();
 		Connection conn=con.conexion();
 		Statement st = null;
 		ResultSet rs = null;
 		st = (Statement)conn.createStatement();
-		String querry = "SELECT d.nombres, a.nombres, a.apellido paterno, a.apellido materno"
-				+ "FROM estudiante a, inscripcion b, materia_has_paralelo c, materia d, materia_has_paralelo_has_decente e"
+		String querry = "SELECT COUNT(a.idestudiante) AS Total"
+				+ "FROM estudiante a, inscripcion b, materia_has_paralelo c, materia d"
+				+ "WHERE a.idestudiante=b.estudiante_idestudiante"
+				+ "AND b.materia_has_paralelo_materia_idmateria=c.materia_idmateria"
+				+ "AND b.materia_has_paralelo_paralelo_idparalelo=c.paralelo_idparalelo"
+				+ "AND c.gestion='"+gestion+"'"
+				+ "AND d.nombres='"+curso+"'";
+		rs = st.executeQuery(querry);
+		return rs;
+	}
+	public ResultSet EstCurso(String curso, String gestion, String paralelo) throws SQLException{
+		DB_Connect con = new DB_Connect();
+		Connection conn=con.conexion();
+		Statement st = null;
+		ResultSet rs = null;
+		st = (Statement)conn.createStatement();
+		String querry = "SELECT COUNT(a.nombres) AS Total"
+				+ "FROM estudiante a, inscripcion b, materia_has_paralelo c, materia d, paralelo e"
 				+ "WHERE a.idestudiante=b.estudiante_idestudiante"
 				+ "AND b.materia_has_paralelo_materia_idmateria=c.materia_idmateria"
 				+ "AND e.materia_has_paralelo_materia_idmateria=d.idmateria"
-				+"AND e.gestion = '"+gestion+"' AND d.nombres = '"+curso+"'";
+				+ "AND c.materia_has_paralelo_idparalelo=e.idparalelo"
+				+ "AND c.gestion = '"+gestion+"'"
+				+ "AND d.nombres = '"+curso+"'"
+				+ "AND e.idParalelo = '"+paralelo+"'";
 		rs = st.executeQuery(querry);
 		return rs;
 	}
