210cdd7357b7ee326cb02cb9339fd5f5f5440cc5
AugustoBet4 <augusto.bet4@gmail.com>
2017-06-28 13:51:50 -0400

    Querrys Agregadas

--- src/modelo/Querry.java
+++ src/modelo/Querry.java
@@ -18,19 +18,32 @@ public class Querry {
 		rs = st.executeQuery(querry);
 		return rs;
 	}
-	public ResultSet EstGen(){
+//	public ResultSet EstGeneral() throws SQLException{
+//		
+//	}
+	public ResultSet EstGenero() throws SQLException{
 		DB_Connect con = new DB_Connect();
 		Connection conn=con.conexion();
 		Statement st = null;
 		ResultSet rs = null;
-		try {
-			st = (Statement)conn.createStatement();
-			String querry = "select * from prueba";
-			rs = st.executeQuery(querry);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		con.closecon();
+		st = (Statement)conn.createStatement();
+		String querry = "SELECT genero, COUNT(genero) AS Total FROM estudiante GROUP BY (genero)";
+		rs = st.executeQuery(querry);
+		return rs;
+	}
+	public ResultSet EstCurso(String curso, String gestion) throws SQLException{
+		DB_Connect con = new DB_Connect();
+		Connection conn=con.conexion();
+		Statement st = null;
+		ResultSet rs = null;
+		st = (Statement)conn.createStatement();
+		String querry = "SELECT d.nombres, a.nombres, a.apellido paterno, a.apellido materno"
+				+ "FROM estudiante a, inscripcion b, materia_has_paralelo c, materia d, materia_has_paralelo_has_decente e"
+				+ "WHERE a.idestudiante=b.estudiante_idestudiante"
+				+ "AND b.materia_has_paralelo_materia_idmateria=c.materia_idmateria"
+				+ "AND e.materia_has_paralelo_materia_idmateria=d.idmateria"
+				+"AND e.gestion = '"+gestion+"' AND d.nombres = '"+curso+"'";
+		rs = st.executeQuery(querry);
 		return rs;
 	}
 }
