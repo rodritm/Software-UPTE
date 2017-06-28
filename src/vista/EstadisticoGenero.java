d5f43db9170bfeb8df67d087a22b47e87136b705
PrimeAndrew <elim_andrew@live.com>
2017-06-28 16:01:51 -0400

    ESTADISTICO GENERO

--- src/vista/EstadisticoGenero.java
+++ src/vista/EstadisticoGenero.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class EstadisticoGenero {
 
@@ -41,10 +42,8 @@ public class EstadisticoGenero {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-
-
 		frame = new JFrame();
-		frame.setBounds(100, 100, 421, 190);
+		frame.setBounds(100, 100, 421, 279);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setLocationRelativeTo(null);
@@ -52,16 +51,32 @@ public class EstadisticoGenero {
 		frame.getContentPane().setBackground(Color.WHITE);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(160, 66, 224, 24);
+		comboBox.setBounds(176, 32, 224, 24);
 		frame.getContentPane().add(comboBox);
 		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(176, 75, 224, 24);
+		frame.getContentPane().add(comboBox_1);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setBounds(176, 116, 224, 24);
+		frame.getContentPane().add(comboBox_2);
+		
 		JButton btnOk = new JButton("OK");
-		btnOk.setBounds(286, 102, 98, 25);
+		btnOk.setBounds(302, 217, 98, 25);
 		frame.getContentPane().add(btnOk);
 		
 		JLabel lblGestion = new JLabel("GESTION:");
-		lblGestion.setBounds(87, 71, 55, 15);
+		lblGestion.setBounds(80, 32, 55, 15);
 		frame.getContentPane().add(lblGestion);
+		
+		JLabel lblCurso = new JLabel("CURSO:");
+		lblCurso.setBounds(80, 75, 55, 15);
+		frame.getContentPane().add(lblCurso);
+		
+		JLabel lblParalelo = new JLabel("PARALELO: ");
+		lblParalelo.setBounds(80, 121, 78, 15);
+		frame.getContentPane().add(lblParalelo);
 		
 		JButton btnAtras = new JButton("ATRAS");
 		btnAtras.addActionListener(new ActionListener() {
@@ -71,8 +86,16 @@ public class EstadisticoGenero {
 				frame.dispose();
 			}
 		});
-		btnAtras.setBounds(87, 102, 98, 25);
+		btnAtras.setBounds(26, 217, 98, 25);
 		frame.getContentPane().add(btnAtras);
+		
+		JLabel lblGenero = new JLabel("GENERO:");
+		lblGenero.setBounds(80, 168, 55, 15);
+		frame.getContentPane().add(lblGenero);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setBounds(176, 163, 224, 24);
+		frame.getContentPane().add(comboBox_3);
 	}
 
 }
