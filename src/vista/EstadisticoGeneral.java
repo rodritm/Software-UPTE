b1a5f8a1ca1485709d0cbea34754c3a39a9e2a91
PrimeAndrew <elim_andrew@live.com>
2017-06-28 15:31:56 -0400

    intefaz estadistico general

---
+++ src/vista/EstadisticoGeneral.java
@@ -0,0 +1,79 @@
+package vista;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class EstadisticoGeneral {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EstadisticoGeneral window = new EstadisticoGeneral();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public EstadisticoGeneral() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+
+
+		frame = new JFrame();
+		frame.setBounds(100, 100, 421, 190);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+		frame.setUndecorated(true);
+		frame.getContentPane().setBackground(Color.WHITE);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(160, 66, 224, 24);
+		frame.getContentPane().add(comboBox);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setBounds(286, 102, 98, 25);
+		frame.getContentPane().add(btnOk);
+		
+		JLabel lblGestion = new JLabel("GESTION:");
+		lblGestion.setBounds(87, 71, 55, 15);
+		frame.getContentPane().add(lblGestion);
+		
+		JButton btnAtras = new JButton("ATRAS");
+		btnAtras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Reportes nuevo1 = new Reportes();
+				nuevo1.main(null);
+				frame.dispose();
+			}
+		});
+		btnAtras.setBounds(87, 102, 98, 25);
+		frame.getContentPane().add(btnAtras);
+	}
+
+}
