8ab5c56c64808b722636be6f54e887e13ea48abf
AugustoBet4 <augusto.bet4@gmail.com>
2017-07-02 21:39:36 -0400

    prueba

--- src/modelo/Querry.java
+++ src/modelo/Querry.java
@@ -132,10 +132,11 @@ public class Querry {
 			pre.setString(1, id);
 			pre.executeUpdate();
 			
+			pre.execute("SET FOREIGN_KEY_CHECKS=0");
 			sql = "DELETE FROM inscripcion WHERE idinscripcion =?";
 			pre.setString(1, id);
 			pre.executeUpdate();
-			
+			pre.execute("SET FOREIGN_KEY_CHECKS=1");
 			con.close();
 		}catch(SQLException e) {
 			e.printStackTrace();
